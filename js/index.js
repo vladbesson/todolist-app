9f3574d8fda9b03bacc67826a16cb103e46bee02
Vladislav Balabanovich <vladislav.svoi@gmail.com>
2021-03-04 19:27:10 +0200

    feat: rendering list from array

--- js/index.js
+++ js/index.js
@@ -6,6 +6,24 @@ const TODO_LIST = [
 	{ title: 'Погулять в парке 🌳' },
 ];
 
-console.log(TODO_LIST);
+const container = document.querySelector('.todo__list');
 
+function renderList() {
+	const result = TODO_LIST.map(function(item) {
+		return `
+			<li class="todo__item task">
+				<div class="task__info">
+					<p class="task__name">${item.title}️</p>
+				</div>
+				<div class="task__controls">
+					<button class="task__btn task__btn_copy" type="button"><img src="./images/duplicate-icon.svg" width="25" height="25" alt="Копировать"></button>
+					<button class="task__btn task__btn_delete" type="button"><img src="./images/delete-icon.svg" width="18" height="17" alt="Удалить"></button>
+				</div>
+			</li>
+		`;
+	}).join('');
 
+	container.insertAdjacentHTML('afterbegin', result);
+}
+
+renderList();
