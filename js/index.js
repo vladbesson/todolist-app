65bb633ceb858edeab14512eeae76aa650e05654
Vladislav Balabanovich <vladislav.svoi@gmail.com>
2021-03-04 20:25:28 +0200

    feat: duplicate anb delete tasks

--- js/index.js
+++ js/index.js
@@ -10,6 +10,37 @@ const container = document.querySelector('.todo__list');
 const todoForm = document.querySelector('.todo__form');
 const templateElement = document.querySelector('.template');
 
+function deleteTaskHandler(evt) {
+	const target = evt.target;
+	const currentTask = target.closest('.task');
+
+	currentTask.remove();
+}
+
+function duplicateTaskHandler(evt) {
+	//найти дом ноду задачи
+	const target = evt.target;
+	const currentTask = target.closest('.task');
+
+	//создать новую дом ноду задачи передав найденный текст
+	const newTask = currentTask.cloneNode(true);
+
+	//навесить на новую задачу обработчики события
+	addTaskListeners(newTask);
+
+	//вставить на страницу
+	container.prepend(newTask);
+}
+
+
+function addTaskListeners(task) {
+	const deleteButton = task.querySelector('.task__btn_delete');
+	deleteButton.addEventListener('click', deleteTaskHandler);
+
+	const duplicateButton = task.querySelector('.task__btn_copy');
+	duplicateButton.addEventListener('click', duplicateTaskHandler);
+}
+
 function createTaskDomNode(item){
 	const newItem = templateElement.content.cloneNode(true);
 	const title = newItem.querySelector('.task__name');
@@ -19,7 +50,11 @@ function createTaskDomNode(item){
 }
 
 function renderList() {
-	const result = TODO_LIST.map(createTaskDomNode);
+	const result = TODO_LIST.map(function(item) {
+		const newTask = createTaskDomNode(item);
+		addTaskListeners(newTask);
+		return newTask;
+	});
 
 	container.append(...result);
 }
@@ -30,6 +65,9 @@ function addTaskFormListener(evt) {
 	const inputTitle = input.value;
 
 	const newTask = createTaskDomNode({ title: inputTitle });
+
+	addTaskListeners(newTask);
+
 	container.prepend(newTask);
 
 	input.value = '';
